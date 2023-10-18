c09b6269de7944dfb0039608418d8d5e040c5cf1
hotchapa <hotchapa@naver.com>
2023-10-18 13:59:51 +0900

    feat : 초기 구축 라우트 설치까지 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient";
 import App from "./App";
 import "./index.css";
+import initMockAPI from "./mocks/index";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/SGlobal";
 
 if (process.env.NODE_ENV === "development") {
-  import("./mocks/browser.js").then(module => {
-    const worker = module.worker;
-    worker.start();
-  });
+  initMockAPI();
 }
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
